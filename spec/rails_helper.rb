31856b271ed142e75da3c866bd9ab999702cd166
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-04-04 15:05:30 +0900

    fix: FactoryBot 重複定義エラーを解決
    - config/environment ロード前に FactoryBot.factories.clear を実行
    - test 環境でのfactories 重複登録を防止

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
+# Clear FactoryBot before loading environment
+FactoryBot.factories.clear
+
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
