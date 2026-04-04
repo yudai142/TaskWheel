90a10083355f2fb777dccc3920ecd4fa5322c448
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-04-04 15:55:03 +0900

    修正: 履歴属性を修正し履歴シリアライザーを追加

--- spec/factories/histories.rb
+++ spec/factories/histories.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :history do
     member { association :member }
     work { association :work }
-    assigned_date { Date.current }
+    date { Date.current }
 
     trait :past do
-      assigned_date { Date.current - 7.days }
+      date { Date.current - 7.days }
     end
 
     trait :future do
-      assigned_date { Date.current + 7.days }
+      date { Date.current + 7.days }
     end
   end
 end
