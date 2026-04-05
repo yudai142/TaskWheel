733f362a2578e507ec355f12f50e05fb19498acd
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-04-05 18:11:34 +0900

    スタイル型定義修正

---
+++ app/javascript/types/css-modules.d.ts
@@ -0,0 +1,10 @@
+// CSS ファイルのインポート型定義
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
